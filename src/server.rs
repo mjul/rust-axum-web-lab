8527018bf27edf45d67f996f11d5633554f160a0
Martin Jul <martin@mjul.com>
2023-08-09 10:13:30 +0200

    Add nested router with application state

--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,14 @@
 //! HTTP-based application server with routes
-use askama::Template;
-use axum::{routing::get, Router};
 use std::net::SocketAddr;
-use tower_http::services::{ServeDir, ServeFile};
+
+use askama::Template;
+use axum::extract::State;
+use axum::handler::Handler;
+use axum::{routing::get, Router};
+use axum_macros::debug_handler;
+use tower_http::services::ServeDir;
 use tower_http::trace::TraceLayer;
-use tracing::{info, info_span, instrument, Instrument};
+use tracing::{instrument, Instrument};
 
 #[derive(Template)] // this will generate the code...
 #[template(path = "index.html")] // using the template in this path, relative to the `templates` dir in the crate root
@@ -17,10 +21,12 @@ async fn index() -> IndexTemplate<'static> {
     IndexTemplate { name: "world" }
 }
 
+#[derive(Clone)]
 struct Language {
     name: &'static str,
     year: u32,
 }
+
 const LANGUAGES: [Language; 6] = [
     Language {
         name: "FORTRAN",
@@ -52,18 +58,61 @@ const LANGUAGES: [Language; 6] = [
 // It also shows how to use loops in the template
 #[derive(Template)]
 #[template(path = "languages/index.html")]
-struct LanguagesTemplate<'a> {
-    languages: &'a [Language], // the field name should match the variable name in the template
+struct LanguagesTemplate {
+    languages: Vec<Language>, // the field name should match the variable name in the template
 }
 
-async fn languages() -> LanguagesTemplate<'static> {
+async fn languages() -> LanguagesTemplate {
     LanguagesTemplate {
-        languages: &LANGUAGES,
+        languages: LANGUAGES.into(),
     }
 }
 
+#[derive(Clone)]
+struct AppState {
+    old_languages: Vec<Language>,
+    new_languages: Vec<Language>,
+}
+
+impl AppState {
+    pub fn new() -> Self {
+        let (old_languages, new_languages) =
+            LANGUAGES.into_iter().clone().partition(|l| l.year < 1970);
+        Self {
+            old_languages,
+            new_languages,
+        }
+    }
+}
+
+// To access the state in the we need to use the `State` extractor like this.
+// Note the `#[debug_handler]` macro - it makes the compiler errors more readable
+// in case the handler is not correctly defined.
+#[debug_handler]
+async fn stateful_old_languages(State(app_state): State<AppState>) -> LanguagesTemplate {
+    LanguagesTemplate {
+        languages: app_state.old_languages.clone(),
+    }
+}
+
+async fn stateful_new_languages(State(app_state): State<AppState>) -> LanguagesTemplate {
+    LanguagesTemplate {
+        languages: app_state.new_languages.clone(),
+    }
+}
+
+fn stateful_router() -> Router<AppState> {
+    Router::<AppState>::new()
+        .route("/old", get(stateful_old_languages))
+        .route("/new", get(stateful_new_languages))
+}
+
 /// Get the application router
-pub(crate) fn router() -> Router {
+// When combining routers, it is simpler to use a generic return type (see `.nest` below)
+pub(crate) fn router<T>() -> Router<T>
+where
+    T: Clone + Send + Sync + 'static,
+{
     Router::new()
         // Route the root to the index fn above
         .route("/", get(index))
@@ -76,6 +125,10 @@ pub(crate) fn router() -> Router {
         // Route the /languages path to the languages fn above
         // This is an example of a using templates with inheritance
         .route("/languages/", get(languages))
+        // We can have state in the application and pass it to the handlers
+        // This changes the signature of the Router and the handler functions
+        .nest("/stateful/", stateful_router())
+        .with_state(AppState::new())
         // Add tracing to the router (i.e. trace all of the above)
         .layer(TraceLayer::new_for_http())
 }
